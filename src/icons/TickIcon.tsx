64fadac2438d9ba8adcf5aa5ce40ca533dd7733e
Muhammad Saqib Ali <MSaqibAliISF@gmail.com>
2024-12-25 13:02:25 +0500

    add TickIcon

---
+++ src/icons/TickIcon.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from "react";
+
+const TickIcon: FC = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="Tick Icon" clipPath="url(#clip0_44_21)">
+        <g id="Group">
+          <g id="Group_2">
+            <path
+              id="Vector"
+              d="M19.7071 2.94907C19.3166 2.55852 18.6835 2.55852 18.2929 2.94907L6.31228 14.9298L1.70713 10.3247C1.31662 9.93411 0.683496 9.93415 0.29291 10.3247C-0.0976367 10.7152 -0.0976367 11.3483 0.29291 11.7388L5.60518 17.051C5.99557 17.4415 6.62916 17.4413 7.01939 17.051L19.7071 4.36329C20.0977 3.97278 20.0976 3.33962 19.7071 2.94907Z"
+              fill="#0F73F6"
+            />
+          </g>
+        </g>
+      </g>
+      <defs>
+        <clipPath id="clip0_44_21">
+          <rect width="20" height="20" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default TickIcon;
