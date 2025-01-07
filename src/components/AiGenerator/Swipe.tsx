4ac56a06bd0d6280b05893da0a0569d01671e0f3
Muhammad Saqib Ali <MSaqibAliISF@gmail.com>
2025-01-07 16:57:54 +0500

    update ui

--- src/components/AiGenerator/Swipe.tsx
+++ src/components/AiGenerator/Swipe.tsx
@@ -18,47 +18,45 @@ const Swipe: FC = () => {
 
   return (
     <div className="p-4 pb-12 flex flex-col w-full">
-      <div className="space-y-6">
-        <Tabs defaultValue="text_and_arrow">
-          <TabsList>
-            <TabsTrigger value="text_and_arrow">Text & Arrow</TabsTrigger>
-            <TabsTrigger value="arrow">Arrow</TabsTrigger>
-          </TabsList>
-          <TabsContent value="text_and_arrow">
-            <div className="grid gap-2">
-              <div className="flex items-center gap-2">
-                <RadioButton
-                  checked={isIntroSlideArrowEnabled}
-                  setChecked={setIsIntroSlideArrowEnabled}
-                  label="Intro Slide Arrow"
-                />
-              </div>
-              <Input type="text" placeholder="Intro Slide Arrow Text" />
+      <Tabs defaultValue="text_and_arrow">
+        <TabsList>
+          <TabsTrigger value="text_and_arrow">Text & Arrow</TabsTrigger>
+          <TabsTrigger value="arrow">Arrow</TabsTrigger>
+        </TabsList>
+        <TabsContent value="text_and_arrow" className="space-y-6">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2">
+              <RadioButton
+                checked={isIntroSlideArrowEnabled}
+                setChecked={setIsIntroSlideArrowEnabled}
+                label="Intro Slide Arrow"
+              />
             </div>
+            <Input type="text" placeholder="Intro Slide Arrow Text" />
+          </div>
 
-            <div className="grid gap-2">
-              <div className="flex items-center gap-2">
-                <RadioButton
-                  checked={isRegularSlideArrowEnabled}
-                  setChecked={setIsRegularSlideArrowEnabled}
-                  label="Regular Slide Arrow"
-                />
-              </div>
-              <Input type="text" placeholder="Regular Slide Arrow Text" />
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2">
+              <RadioButton
+                checked={isRegularSlideArrowEnabled}
+                setChecked={setIsRegularSlideArrowEnabled}
+                label="Regular Slide Arrow"
+              />
             </div>
-          </TabsContent>
-          <TabsContent value="arrow">
-            <div className="px-2 grid grid-cols-4 gap-x-1 gap-y-4">
-              <div style={{ color: "rgb(0, 0, 0)" }}>
-                <Arrow1 />
-              </div>
-              <div style={{ color: "rgb(0, 0, 0)" }}>
-                <Arrow2 />
-              </div>
+            <Input type="text" placeholder="Regular Slide Arrow Text" />
+          </div>
+        </TabsContent>
+        <TabsContent value="arrow">
+          <div className="px-2 grid grid-cols-4 gap-x-1 gap-y-4">
+            <div style={{ color: "rgb(0, 0, 0)" }}>
+              <Arrow1 />
             </div>
-          </TabsContent>
-        </Tabs>
-      </div>
+            <div style={{ color: "rgb(0, 0, 0)" }}>
+              <Arrow2 />
+            </div>
+          </div>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 };
